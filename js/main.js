7c37c09a453c2d51c588be7772878a1f428771d3
Behruz01 <ibragimovbehruz822@gmail.com>
2023-02-05 21:13:24 +0500

    swiper

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ export const getData = async function getData(select) {
 
   renderProduct(products);
 };
-getData();
+// getData();
 
 // options;
 
@@ -89,3 +89,43 @@ elSelect.addEventListener("change", (evt) => {
   }
 });
 export default getData;
+
+// swiper
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: "vertical",
+  loop: true,
+  slidesPerView: 3,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
